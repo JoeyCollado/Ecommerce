9b3214ee15e616b42e0fe30a9eb04c2caca995a7
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-11-23 18:47:11 +0800

    mongo db connection function and setup done

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
-  title: "MarkCart",
+  title: "CartMark",
   description: "E-Commerce with Next.js ",
 };
 
--- config/db.ts
+++ config/db.ts
@@ -1,3 +1,29 @@
+import mongoose, { mongo } from "mongoose";
 //mongodb configuration
 
 //npm i mongoose
+
+let cached = global.mongoose
+
+if(!cached){
+    cached = global.mongoose = {connection: null, promise: null}
+}
+
+async function connectDB(){ //async function to establish connection to mongodb
+   if(cached.connection){ //if connection is true
+     return cached.connection
+   }
+   if(!cached.promise){ //if not establish the connection
+     const options = {
+        bufferCommands:false
+     }
+     cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/markcart`, options).then(mongoose => {
+        return mongoose // once connection established return mongoose
+     })
+   }
+
+   cached.connection = await cached.promise
+   return cached.connection
+}
+
+export default connectDB
