8d301944a2270a66d6718431a28e2cd2498764e9
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-11-24 18:48:21 +0800

    Create function

--- clerk-setup/webhook setup/inggest setup/readme.md
+++ clerk-setup/webhook setup/inggest setup/readme.md
@@ -4,3 +4,4 @@ do this step after 3rd step of webhook setup
 npm install inngest
 
 https://app.inngest.com/env/production/onboarding/create-app
+https://www.inngest.com/docs?ref=support-center
---
+++ config/inngest.ts
@@ -0,0 +1,30 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "markcart-next" }); // we can use this to create functions
+
+//functions to manage clerk webhooks
+
+//inngest function to save user data to a database
+export const syncUserCreation = inngest.createFunction(
+    {
+        id: 'sync-user-from-clerk'
+    },
+    {event: 'clerk/user.created'},
+    async ({event}) => {
+       const {id, first_name, last_name, email_addresses, image_url} = event.data //destructure from the event data
+       //create object where we will define user data then store it to db
+       const userData = {
+        _id:id,
+        email: email_addresses[0].email_addresses,
+        name: first_name + ' ' + last_name,
+        image_url: image_url
+       }
+       //save data
+       await connectDB()
+       await User.create(userData)
+    }
+)
+
