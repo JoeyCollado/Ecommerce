f1f43006372c89dfd7df2792919902afea6adcde
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-12-09 18:57:48 +0800

    api to add user address to database done

--- app/api/user/add-address/route.js
+++ app/api/user/add-address/route.js
@@ -1,12 +1,25 @@
 //api to add address
 
+import connectDB from "@/config/db";
+import Address from "@/models/Address";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
 export async function POST(request){
 
     try{
 
-        //create new model
+        //get user id
+        const {userId} = getAuth(request)
+        //get address from request
+        const  {address} = await request.json()
+        //save to db
+        await connectDB()
+        const newAddress = await Address.create({...address, userId})
+
+        return NextResponse.json({success: true, message: 'Address added successfully', newAddress})
 
     }catch(error){
-
+      return NextResponse.json({success: false, message: error.message});
     }
 }
