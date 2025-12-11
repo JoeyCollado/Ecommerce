9f30987714dc72f1b57ec415c53abd7163c0f551
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-12-11 12:13:20 +0800

    create order api done

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,5 +1,6 @@
 // api to create new order
 
+import { inngest } from "@/config/inngest";
 import Product from "@/models/Product";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
@@ -20,8 +21,29 @@ export async function POST(request){
         return acc + product.offerPrice * item.quantity
       }, 0) //initial value
       //create order
-      
+
+      //send event to inngest
+      await inngest.send({
+        name: 'order/created',
+        data: {
+            userId,
+            address,
+            items,
+            amount: amount + Math.floor(amount * 0.02),
+            date: Date.now()
+        }
+      })
+
+      //clear users Cart
+      const user = await User.findById(userId)
+      user.cartItems = {}
+      await user.save()
+      //send response
+      return NextResponse.json({success: true, message: 'Order Placed'})
+
     }catch(error){
+        console.log(error)
+        return NextResponse.json({success: false, message: error.message})
 
     }
 }
